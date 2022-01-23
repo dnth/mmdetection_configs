dec87da435e67cdc505f15138ee40c9be2ad6acf
dnth <dickson.neoh@gmail.com>
2022-01-23 18:21:16 +0800

    update small

--- configs/swin/vfnet_swin_small_patch4_window7_224.py
+++ configs/swin/vfnet_swin_small_patch4_window7_224.py
@@ -1,8 +1,16 @@
-_base_ = [
-    './vfnet_swin_tiny_patch4_window7_224.py'
-]
+_base_ = ["./vfnet_swin_tiny_patch4_window7_224.py"]
+
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224.pth'  # noqa
 
 model = dict(
     backbone=dict(
-        depths=[2, 2, 18, 2]))
-
+        embed_dims=128,
+        depths=[2, 2, 18, 2],
+        num_heads=[4, 8, 16, 32],
+        out_indices=(0, 1, 2, 3),
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained),
+    ),
+    neck=dict(
+        in_channels=[128, 256, 512, 1024],
+    ),
+)
