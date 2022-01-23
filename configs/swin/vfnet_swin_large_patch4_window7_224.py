17aa5e3b9707947cefa423e92784d3d5755d6261
dnth <dickson.neoh@gmail.com>
2022-01-23 18:07:54 +0800

    add base

---
+++ configs/swin/vfnet_swin_large_patch4_window7_224.py
@@ -0,0 +1,16 @@
+_base_ = ["./vfnet_swin_tiny_patch4_window7_224.py"]
+
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window7_224_22k.pth'  # noqa
+
+model = dict(
+    backbone=dict(
+        embed_dims=192,
+        depths=[2, 2, 18, 2],
+        num_heads=[6, 12, 24, 48],
+        out_indices=(0, 1, 2, 3),
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained),
+    ),
+    neck=dict(
+        in_channels=[192, 256, 512, 1024],
+    ),
+)
