98ffadec0d8118580091f91a3b7310f6967cbeef
Dickson Neoh Tze How <dickson.neoh@gmail.com>
2022-01-24 14:09:21 +0800

    Update vfnet_swin_small_patch4_window7_224.py

--- configs/swin/vfnet_swin_small_patch4_window7_224.py
+++ configs/swin/vfnet_swin_small_patch4_window7_224.py
@@ -4,13 +4,7 @@ pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.
 
 model = dict(
     backbone=dict(
-        embed_dims=128,
         depths=[2, 2, 18, 2],
-        num_heads=[4, 8, 16, 32],
-        out_indices=(0, 1, 2, 3),
         init_cfg=dict(type='Pretrained', checkpoint=pretrained),
-    ),
-    neck=dict(
-        in_channels=[128, 256, 512, 1024],
     ),
 )
