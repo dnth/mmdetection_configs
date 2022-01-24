4c06d25bcf30afa767a3b71db8d6db1fe393477d
dnth <dickson.neoh@gmail.com>
2022-01-24 15:04:56 +0800

    standarize names

--- configs/swin/vfnet_swin-l-p4-w7_fpn_1x_coco.py
+++ configs/swin/vfnet_swin-l-p4-w7_fpn_1x_coco.py
@@ -1,4 +1,4 @@
-_base_ = ["./vfnet_swin_tiny_patch4_window7_224.py"]
+_base_ = ["./vfnet_swin-t-p4-w7_fpn_1x_coco.py"]
 
 pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window7_224_22k.pth'  # noqa
 
