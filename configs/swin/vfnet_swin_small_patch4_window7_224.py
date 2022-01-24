5439cf1f455317cc6c9f2d1035d586625dd0913d
Dickson Neoh Tze How <dickson.neoh@gmail.com>
2022-01-24 14:06:33 +0800

    Update vfnet_swin_small_patch4_window7_224.py

--- configs/swin/vfnet_swin_small_patch4_window7_224.py
+++ configs/swin/vfnet_swin_small_patch4_window7_224.py
@@ -1,6 +1,6 @@
 _base_ = ["./vfnet_swin_tiny_patch4_window7_224.py"]
 
-pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224.pth'  # noqa
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth'  # noqa
 
 model = dict(
     backbone=dict(
